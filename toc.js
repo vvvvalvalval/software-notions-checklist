79627d048d188ecd02618dc2528551a4d6a35e23
Valentin Waeselynck <val@bandsquare.fr>
2016-12-17 14:02:16 +0100

    generated ToC in README

--- toc.js
+++ toc.js
@@ -1,10 +1,9 @@
+// run this file to write the table of contents in README.md
 var fs = require('fs');
 var _ = require('underscore');
 var txt = fs.readFileSync('./README.md', 'utf8');
 
 var arr = txt.match(/([^]*Table.of.Contents\s*)([^]*)(##.General.notions[^]*)/);
-
-console.log("arr",arr); // FIXME clean
 
 function slugifyHeading(h){
   return h.toLowerCase().replace(/[^\w\s]+/g,'').replace(/\s/g,'-');
@@ -16,7 +15,6 @@ function repeat(n, v){
 
 (function (_all,p1,p2,body) {
 
-  console.log("body",body); // FIXME clean
   var toc = body.match(/##.*/g)
     .map(function (line) {
       var text = line.match(/#*\s+(.*)/)[1];
@@ -26,10 +24,11 @@ function repeat(n, v){
       return {line: line, text: text, anchor: anchor, level: level};
     })
     .map(function (m) {
-      return repeat(m.level - 2, '  ').join('') + '* [' + m.text + '](' + m.anchor + ')';
+      return repeat(m.level - 2, '  ').join('') + '* [' + m.text + '](#' + m.anchor + ')';
     })
     .join('\n');
 
-  console.log("toc", toc); // FIXME clean
+  var out = p1 + toc + '\n\n' + body;
+  fs.writeFileSync('./README.md', out, {encoding: 'utf8'});
 
 }.apply(null, arr));
